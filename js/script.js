afda9e885e97f46a9d6e3345e45aa3e81e1843a7
Claudivan <67664671+ClaudivanSantos@users.noreply.github.com>
2023-06-13 22:23:56 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ function checkDevice() {
     navigator.userAgent.match(/Windows Phone/i)
   ) {
     // está utilizando celular
-    pipe.style.animationDuration = "1.0s";
+    pipe.style.animationDuration = "0.5s";
     sound.style.width = "50px";
     sound.style.height = "50px";
 
